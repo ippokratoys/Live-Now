df3309c341ae1adf84bc888cf599e5ef9ca31e09
apostolos107 <apostolos107@gmail.com>
2017-08-26 20:13:13 +0300

    I found the problem

--- src/main/java/application/search/SearchService.java
+++ src/main/java/application/search/SearchService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
+import java.util.Iterator;
 import java.util.List;
 import javax.persistence.Query;
 
@@ -22,11 +23,21 @@ public class SearchService {
         String queryStr = search.buildQuery();
         Query query=entityManager.createNativeQuery(queryStr);
         search.passParameter(queryStr,query);
-        List<Apartment> apartments = query.getResultList();
-        for (Apartment oneApartment : apartments){
-            oneApartment.toString();
-            System.out.printf("-------------------------------\n");
+        List<Object> apartments = (List<Object>) query.getResultList();
+        Iterator itr = apartments.iterator();
+        while(itr.hasNext()){
+            Object[] obj = (Object[]) itr.next();
+            //now you have one array of Object for each row
+            Integer apartment_id = Integer.parseInt(String.valueOf(obj[0]));
+            System.out.println(apartment_id);
+//            String client = String.valueOf(obj[0]);
         }
+//        System.out.println(apartments.get(0));
+//        for (String oneApartment : apartments){
+//            System.out.println(oneApartment);
+//            oneApartment.toString();
+//            System.out.printf("-------------------------------\n");
+//        }
         return null;
     }
 }
