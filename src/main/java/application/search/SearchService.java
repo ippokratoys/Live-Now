8f400369cfd5144c2658c9d6e00dab2949066a27
apostolos107 <apostolos107@gmail.com>
2017-08-29 19:41:17 +0300

    Now the results page is ready to run (remove the not available and booked apartments)

--- src/main/java/application/search/SearchService.java
+++ src/main/java/application/search/SearchService.java
@@ -2,6 +2,7 @@ package application.search;
 
 import application.database.Apartment;
 import application.database.Availability;
+import application.database.BookInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
@@ -30,14 +31,24 @@ public class SearchService {
         String queryStrAvailability="Select * from availability where availability.apartment_apartment_id=:aparId and (:startDate between availability.from_av and availability.to_av) and (:endDate between availability.from_av and availability.to_av)";
         Query queryAvailability=entityManager.createNativeQuery(queryStrAvailability,Availability.class);
 
+        String queryStrBook="Select * from book_info where book_info.apartment=:aparId and (:startDate between book_info.book_in and book_info.book_out) and (:endDate between book_info.book_in and book_info.book_out)";
+        Query queryBook=entityManager.createNativeQuery(queryStrBook, BookInfo.class);
+
         List<Apartment> apartmentsResultsFinal=new ArrayList<Apartment>();
         for (Apartment oneApartment :
                 apartmentsResult) {
             queryAvailability.setParameter("aparId",oneApartment.getApartmentId());
             queryAvailability.setParameter("startDate",search.getFromDate());
             queryAvailability.setParameter("endDate",search.getToDate());
+            queryBook.setParameter("aparId",oneApartment.getApartmentId());
+            queryBook.setParameter("startDate",search.getFromDate());
+            queryBook.setParameter("endDate",search.getToDate());
+
+
             List<Availability> availabilityResult = queryAvailability.getResultList();
-            if(availabilityResult.size()<1){
+            List<BookInfo> bookResult = queryBook.getResultList();
+
+            if(availabilityResult.size()<1 || bookResult.size()>0){
                 System.out.println("not available");
                 System.out.println(oneApartment.toString());
             }else{
