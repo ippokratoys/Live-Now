0882b22179ebd423fb29fa4a0fa196689ab21183
apostolos107 <apostolos107@gmail.com>
2017-08-29 17:52:24 +0300

    now it's good

--- src/main/java/application/search/SearchService.java
+++ src/main/java/application/search/SearchService.java
@@ -5,11 +5,7 @@ import application.database.Availability;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 import javax.persistence.Query;
 
 /**
@@ -22,19 +18,6 @@ public class SearchService {
     EntityManager entityManager;
 
 
-    public String fixDate(String date){
-        String newDate;
-        String[] parts=date.split(" ");
-        Date tempDate = null;
-        tempDate = new SimpleDateFormat("MMM", Locale.ENGLISH).parse(parts[1]);
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(tempDate);
-        int month = cal.get(Calendar.MONTH);
-        System.out.println(month == Calendar.FEBRUARY);
-        newDate=parts[5]+month+"/"+parts[2];
-        return newDate;
-    }
-
 // page : current page from the result list (STARTS FROM 1)
     public Result getResultList(Search search,int page){
         String queryStr = search.buildQuery();
@@ -43,25 +26,24 @@ public class SearchService {
         List<Apartment> apartmentsResult = query.getResultList();
         System.out.println(apartmentsResult.size());
 
-        String startDate=fixDate(search.getFromDate().toString());
-        String endDate=fixDate(search.getToDate().toString());
-
         //////chech the availability
         String queryStrAvailability="Select * from availability where availability.apartment_apartment_id=:aparId and (:startDate between availability.from_av and availability.to_av) and (:endDate between availability.from_av and availability.to_av)";
         Query queryAvailability=entityManager.createNativeQuery(queryStrAvailability,Availability.class);
+
+        List<Apartment> apartmentsResultsFinal=new ArrayList<Apartment>();
         for (Apartment oneApartment :
                 apartmentsResult) {
             queryAvailability.setParameter("aparId",oneApartment.getApartmentId());
-            queryAvailability.setParameter("startDate",startDate);
-            queryAvailability.setParameter("endDate",endDate);
+            queryAvailability.setParameter("startDate",search.getFromDate());
+            queryAvailability.setParameter("endDate",search.getToDate());
             List<Availability> availabilityResult = queryAvailability.getResultList();
             if(availabilityResult.size()<1){
-                apartmentsResult.remove(oneApartment);
                 System.out.println("not available");
                 System.out.println(oneApartment.toString());
             }else{
                 System.out.println("Available");
                 System.out.println(oneApartment.toString());
+                apartmentsResultsFinal.add(oneApartment);
             }
         }
         System.out.println("\nend of results\n");
