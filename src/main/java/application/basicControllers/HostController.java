f9e4b32f6f10fbe0e589f34a3dd5478cb44483cc
ippokratoys <polydorosthasis@hotmail.com>
2017-09-01 02:04:31 +0300

    added the capbality of ajax adding dates from the host profile page

--- src/main/java/application/basicControllers/HostController.java
+++ src/main/java/application/basicControllers/HostController.java
@@ -1,27 +1,39 @@
 package application.basicControllers;
 
 import application.database.Apartment;
+import application.database.Availability;
 import application.database.repositories.ApartmentRepository;
 import application.database.repositories.LoginRepository;
+import application.services.AvailabilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by thanasis on 23/8/2017.
  */
-@Controller()
+@Controller
 public class HostController {
+    SimpleDateFormat dateFormat=new SimpleDateFormat("MM/dd/yyy");
+    {
+        dateFormat=new SimpleDateFormat("MM/dd/yyy");
+    }
+
+    @Autowired
+    AvailabilityService availabilityService;
     @Autowired
     LoginRepository loginRepository;
+    @Autowired
+    private ApartmentRepository apartmentRepository;
 
     @RequestMapping(value = "/profile/host/add_apartment",method = RequestMethod.POST)
     String postAddApartmentController(Model model,
@@ -46,9 +58,71 @@ public class HostController {
     @RequestMapping(value = "/profile/host/apartments",method = RequestMethod.GET)
     String getApartmetns(Model model,
                          @AuthenticationPrincipal final UserDetails userDetails
-                         ){
+    ){
+        if(userDetails==null){
+            return "redirect:/login";
+        }
         List<Apartment> apartments = loginRepository.findOne(userDetails.getUsername()).getApartments();
         model.addAttribute("apartments",apartments);
         return "apartments";
+    }
+
+    @RequestMapping(value = "/profile/host/apartment/dates/{apartment_id}",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    List<Availability> getExistingDates(@AuthenticationPrincipal final UserDetails userDetails,
+                                        @PathVariable(value = "apartment_id") int apartmentId
+
+    ){
+        if(userDetails==null){
+            System.out.println("it's null");
+            return null;
+        }else {
+            System.out.println(userDetails.getUsername());
+        }
+        Apartment apartment = apartmentRepository.findOne(apartmentId);
+        if(apartment==null){
+            System.out.println("No apartment with id "+apartmentId);
+            return null;
+        }
+        System.out.println(apartment.getAvailabilities().size());
+        return apartment.getAvailabilities();
+    }
+
+    @RequestMapping(value = "profile/host/apartment/dates/{apartment_id}",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    Boolean addDate(@AuthenticationPrincipal final UserDetails userDetails,
+                   @PathVariable(value = "apartment_id") int apartmentId,
+                   @RequestParam("date_range") String dateStr
+    ){
+        if(userDetails==null){
+            return false;
+        }
+        System.out.println("userDetails = [" + userDetails + "], apartmentId = [" + apartmentId + "], dateStr = [" + dateStr + "]");
+        Date from = null;
+        Date to = null;
+        if(dateStr==null || dateStr.trim().equals("")){
+            System.out.println("Date not exists");
+        }
+        String buff[] = dateStr.split("-");
+        try{
+            from=dateFormat.parse(buff[0]);
+            to=dateFormat.parse(buff[1]);
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        try{
+            Apartment apartment = apartmentRepository.findOne(apartmentId);
+            Availability availability = new Availability(from,to,apartment);
+            if( !apartment.getLogin().getUsername().equals(userDetails.getUsername()) ){
+                System.out.println("not yours hotel "+ userDetails.getUsername());
+                return false;
+            }
+            availabilityService.createAvailability(apartment,availability);
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -10,6 +10,31 @@ function editInfo() {
     $("input[type='submit']").removeClass("hidden");
 };
 
+function addDates(apartmentId) {
+    cur_apartment_id=apartmentId;
+    $("#add_actual").addClass("hidden");
+    $("#add_loading").removeClass("hidden");
+    $.getJSON("/profile/host/apartment/dates/"+apartmentId,function (data) {
+        $("#dates_table").html("<tr>"+
+            "<th class='text-center'>From</th>"+
+            "<th class='text-center'>To</th>"+
+            "</tr>");
+        if(data=="" || data ==null){
+            return ;
+        }
+        var i;
+        for(i=0;i<data.length;i++){
+            $("#dates_table").append(
+                "<tr>" +
+                "<td class='text-center'>"+data[i].fromAv+"</td>" +
+                "<td class='text-center'>"+data[i].toAv+"</td>" +
+                "</tr>");
+        }
+        $("#add_actual").removeClass("hidden");
+        $("#add_loading").addClass("hidden");
+    })
+};
+
 function editApartment(apartmentId) {
     $("#edit_actual").addClass("hidden");
     $("#edit_loading").removeClass("hidden");
