7d36faffe014ba12884140c47ed8c6aaa052ff76
apostolos107 <apostolos107@gmail.com>
2017-09-21 22:54:43 +0300

    nothing

--- src/main/java/application/basicControllers/FileController.java
+++ src/main/java/application/basicControllers/FileController.java
@@ -1,4 +1,15 @@
 package application.basicControllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class FileController {
+//    @RequestMapping("file/profile/{fileName:.+}")
+//    ResponseEntity<Resource> getProfilePic(@PathVariable String fileName){
+//
+//    }
 }
